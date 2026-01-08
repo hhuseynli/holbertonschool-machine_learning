ac3dc52a1778626feae3f2c80da6a5d1191811f2
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-08 14:40:12 +0000

    Task 4: Numpy array

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -3,10 +3,15 @@
 
 
 def array(df):
+    """
+    Input: Dataframe
+
+    Output: ndarray
+    """
     # Select last 10 rows
     df = df[["High", "Close"]].tail(10)
 
     # Convert to ndarray
-    arr = to_numpy(df)
+    arr = df.to_numpy()
 
     return arr
