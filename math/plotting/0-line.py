5dc5737915c0c70783ef56dcb011ee8f1490d968
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-10 12:30:40 -0400

    Task 0: Plot x^3 (red, 0<=x<=10)

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+""" Plot x^3 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """ Set bounds to 0<=x<=10 and color to red """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.xlim(0, 10)
