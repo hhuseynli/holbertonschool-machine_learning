667aa4a5474948d3983576e84e62e24728cb78a9
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-13 18:43:22 +0000

    Task 9: Sum of i squared

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" Calculates a sum """
+""" Sum of i^2 """
 
 
 def summation_i_squared(n):
-    """ let's not use computational power"""
-    """ because we're cool """
-    return n*(n+1)*(2*n+1)/6
+    """ First check the type then return """
+    if isinstance(n, int) and n > 0:
+        return n * (n + 1) * (2 * n + 1) / 6
+    else:
+        return None
