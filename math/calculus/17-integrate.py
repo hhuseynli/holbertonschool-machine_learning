57db4e4775564c50237e3415f6239929f0f3f3fb
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-13 19:40:49 +0000

    Task 17: Integrate polynomials

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Integrate coefficents """
+
+
+def poly_integral(poly, C=0):
+    """ Integration of polynomials """
+    if type(poly) is not list:
+        return None
+    elif not poly:
+        return None
+    elif len(poly) == 0:
+        return None
+    
+    inte = list(C)
+    for i in range(len(poly)):
+        inte.append(poly[i] / (i+1))
+    return inte
