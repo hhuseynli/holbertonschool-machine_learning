479cd9cc0d585b09eb9c996fd4f32666d66f70db
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-13 19:01:55 +0000

    Task 10: Differentiate polynomials

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Differentiate and get coefficients """
+
+
+def poly_derivative(poly):
+    """ Return derivative of a polynomial """
+    if not isinstance(poly, list):
+        return None
+    diff = []
+    total = 0
+    for i in range(1, len(poly)):
+        coefficent = i * poly[i]
+        total += coefficient
+        diff.append(coefficient)
+    if total == 0:
+        return [0]
+    return diff
