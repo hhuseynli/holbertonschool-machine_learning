e18777d5e4465dbccc6d913b35ac6b017f30cf96
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-22 08:25:16 +0000

    Task 7: Concat 2D arrays

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,6 +6,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """ Concat based on an axis """
     cols = len(mat1[0])
     rows = len(mat1)
+    if (rows, cols) != (len(mat2[0]), len(mat2)):
+        return None
     if axis == 0:
         return mat1 + mat2
     elif axis == 1:
