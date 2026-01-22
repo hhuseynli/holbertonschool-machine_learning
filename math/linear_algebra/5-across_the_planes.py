22147e846f744e64f1cf8797ad3f9d7d492dac40
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-22 07:57:38 +0000

    Task 5: Add two-dimensional arrays

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,4 +8,6 @@ def add_matrices2D(mat1, mat2):
     shape2 = (len(mat2), len(mat2[0]))
     if shape1 != shape2:
         return None
-    return [[mat1[i][j] + mat2[i][j] for j in range(shape1[1])] for i in range(shape1[0])]
+    c = len(shape1[1])
+    r = len(shape1[0])
+    return [[mat1[i][j] + mat2[i][j] for j in range(c)] for i in range(r)]
