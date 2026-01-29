547ae780cc89a44aa5f6e7d412827b5c5c4066fa
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-01-29 10:15:25 +0000

    Task 8: Matrix Multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,7 @@
 
 def mat_mul(mat1, mat2):
     """ Get dot product with transverse """
-    if (len(mat1[0]) == len(mat2)):
+    if (len(mat1[0]) != len(mat2)):
         return None
     return [
         [
