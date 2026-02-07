3d3cac8a3f48c07fa6fffef9ce3986c4f2ce8f85
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-02-07 18:31:07 +0000

    Task 4: Inverse of a matrix

---
+++ math/advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Find the inverse of a matrix """
+determinant = __import__("0-determinant").determinant
+adjugate = __import__("4-adjugate").adjugate
+
+
+def inverse(matrix):
+    det = determinant(matrix)
+    adj = adjugate(matrix)
+    if det == 0:
+        return None
+
+    row_n = len(matrix)
+    col_n = len(matrix[0])
+
+    return [[adj[i][j] / det for j in range(col_n)] for i in range(row_n)]
