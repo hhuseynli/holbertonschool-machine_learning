9d245934fd3ddb183b78a9c8c85658d0f71ab9a2
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-02-07 18:34:16 +0000

    Task 4: Inverse of a matrix

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -6,6 +6,15 @@ adjugate = __import__("3-adjugate").adjugate
 
 def inverse(matrix):
     """ Calculate the inverse """
+    if not isinstance(matrix, list):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        raise ValueError('matrix must be a non-empty square matrix')
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError('matrix must be a list of lists')
+        if len(matrix) != len(matrix[i]):
+            raise ValueError('matrix must be a non-empty square matrix')
     det = determinant(matrix)
     adj = adjugate(matrix)
     if det == 0:
