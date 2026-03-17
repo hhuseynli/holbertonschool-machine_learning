2123f1f86b0dbc25a9ab36981fb5c6c41223c1c0
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-03-17 19:07:29 +0000

    Task 0: Create a confusion matrix

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+""" Make a confusion matrix """
+
+
+def create_confusion_matrix(labels, logits):
+    """ input: labels and predictions (m, classes) shaped
+    output: confusion matrix(classes, classes) shaped """
+    classes = labels.shape[1]
+    confusion = np.zeros((classes, classes))
+
+    actual = np.argmax(labels, axis=1)
+    prediction = np.argmax(logits, axis=1)
+
+    np.add.at(confusion, (true, pred), 1)
+    return confusion
