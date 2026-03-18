f4b7568a98c47c709a1345dab1691d2ed3f72d16
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-03-18 09:00:48 +0000

    Task 3: Specificity

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -6,10 +6,8 @@ import numpy as np
 def specificity(confusion):
     """ in: confusion matrix, out: specificity for each class """
     total = confusion.sum()
-    
     tp = np.diag(confusion)
     fn = confusion.sum(axis=1) - tp
     fp = confusion.sum(axis=0) - tp
     tn = total - (tp + fn + fp)
-    
     return tn / (tn + fp)
