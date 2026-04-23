952fefb9a7ac65e765b1481e677595c3b57e2ab4
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-04-23 22:41:46 +0400

    Task 1: Style validation

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -14,15 +14,15 @@ class Neuron:
         self.__W = np.random.randn(nx).reshape(1, nx)
         self.__b = 0
         self.__A = 0
-    
+
     @property
     def W(self):
         return self.__W
-    
+
     @property
     def b(self):
         return self.__b
-    
+
     @property
     def A(self):
         return self.__A
