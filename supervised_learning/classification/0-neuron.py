6219af3f2fc73c9b86f8e6b443b3607ffa920ccd
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-04-23 22:40:49 +0400

    Task 1: Add getters

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -11,18 +11,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.randn(nx).reshape(1, nx)
-        self.__b = 0
-        self.__A = 0
-    
-    @property
-    def W(self):
-        return self.__W
-    
-    @property
-    def b(self):
-        return self.__b
-    
-    @property
-    def A(self):
-        return self.__A
+        self.W = np.random.randn(nx).reshape(1, nx)
+        self.b = 0
+        self.A = 0
---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" 1. Neuron """
+import numpy as np
+
+
+class Neuron:
+    """ A neuron is the simplest building block of a neural network."""
+    def __init__(self, nx):
+        """ Validate input and set private attributes of the neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(nx).reshape(1, nx)
+        self.__b = 0
+        self.__A = 0
+    
+    @property
+    def W(self):
+        return self.__W
+    
+    @property
+    def b(self):
+        return self.__b
+    
+    @property
+    def A(self):
+        return self.__A
