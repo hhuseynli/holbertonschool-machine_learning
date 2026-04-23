e62085a44c594d2ce3cff63e5a5fbeb333ffd1f4
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-04-23 22:33:22 +0400

    Task 0: Add docstring

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -4,8 +4,9 @@ import numpy as np
 
 
 class Neuron:
+    """ A neuron is the simplest building block of a neural network."""
     def __init__(self, nx):
-        """ Validate input and set public attributes. """
+        """ Validate input and set public attributes of the neuron."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
