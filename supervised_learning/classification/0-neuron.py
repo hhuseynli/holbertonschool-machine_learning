9c694d356994c6fa068af9bb11a8bd75060b6012
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-04-23 22:24:41 +0400

    Task 0: Define a Neuron

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -10,6 +10,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(nx)
+        self.W = np.random.randn(nx).reshape(nx, 1)
         self.b = 0
         self.A = 0
