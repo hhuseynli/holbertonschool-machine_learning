f1a1da5dfd12c70bff52b6adbfb9e092b1e54632
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-10 14:43:38 +0400

    Task 12: Evaluate Neural Network

---
+++ supervised_learning/classification/12-neural_network.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+""" Neural network"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ A neural network with one input, one hidden, and one output layer. """
+    def __init__(self, nx, nodes):
+        """ Args:
+                nx => number of input features
+                nodes => number of nodes in the hidden layer of the NN
+        """
+
+        # Input Validation
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Initialize output layer
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    def forward_prop(self, X):
+        """
+        Args:
+            X -> ndarray (nx, m), where nx = features; m = examples
+
+        Returns: __A1 and __A2 -- activations of hidden and output layers
+        """
+
+        # (nodes, nx) @ (nx, m) = (nodes, m)
+        # (nodes, m) + (1, m)[broadcasted] => (nodes, m)
+        z1 = self.__W1 @ X + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+
+        # (1, nodes) @ (nodes, m) = (1, m)
+        z2 = self.__W2 @ self.__A1 + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """ Calculate the cost using binary cross-entropy loss
+        Args:
+            Y => ndarray (1, m) with the correct labels
+            A => ndarray (1, m) with the activated output
+
+        Returns:
+            cost (BCE)
+        """
+        losses = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -np.mean(losses)
+        return cost
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neural network's predictions """
+        _, A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        pred = (A >= 0.5).astype(int)
+        return pred, cost
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
