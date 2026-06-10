274635334134b17ff5c611c5afc7687a714eb075
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-10 14:00:08 +0400

    Task 9: Privatize Neural Network

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Neural network"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ A neural network with one input, one hidden, and one output layer. """
+    def __init__(self, nx, nodes):
+        """ Args:
+                nx => number of input features
+                nodes => number of nodes in the hidden layer of the NN
+        """
+
+        # Input Validation
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Initialize output layer
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
