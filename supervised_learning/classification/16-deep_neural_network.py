301a23fc0bfdeedcf81675eff37636213d481970
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-14 10:31:03 +0400

    Task 16: Deep Neural Network

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,27 +6,25 @@ import numpy as np
 class DeepNeuralNetwork:
     """ A deep neural network consists of multiple hidden layers"""
     def __init__(self, nx, layers):
-        if not isinstance(nx, int):
+        if not (isinstance(nx, int)):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        if (not isinstance(layers, list)) or layers == []:
-            raise TypeError("layer must be a list of positive integers")
-        if (np.array(layers) > 0).sum() != len(layers):
+        if not (isinstance(layers, list)) or layers == []:
             raise TypeError("layers must be a list of positive integers")
-
         self.L = len(layers)
-        self.cache = dict()
-        self.weights = dict()
-
+        self.cache = {}
+        self.weights = {}
+        # L = 3     1, 2
         for layer in range(self.L):
+            if layers[layer] <= 0 or not (isinstance(layer, int)):
+                raise TypeError("layers must be a list of positive integers")
             if layer == 0:
                 prev = nx
             else:
                 prev = layers[layer-1]
-            W = np.random.randn(layers[layer], prev) * np.sqrt(2 / prev)
+            W = (np.random.randn(layers[layer], prev) *
+                 np.sqrt(2 / prev))
             b = np.zeros((layers[layer], 1))
-
-            self.weights[f"W{layer+1}"] = W
-            self.weights[f"b{layer+1}"] = b
+            self.weights.update({f"W{layer+1}": W})
+            self.weights.update({f"b{layer+1}": b})
