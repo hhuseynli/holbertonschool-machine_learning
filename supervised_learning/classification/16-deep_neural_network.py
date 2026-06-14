0c1947bc37ee6c936e171d5631a798b05a300f22
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-14 09:53:13 +0400

    Task 16: Deep Neural Network

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"Deep Neural Network"
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layer must be a list of positive integers")
+        if (np.array(layers) > 0).sum() != len(layers):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = dict()
+        self.weights = dict()
+
+        for layer in range(self.L):
+            if layer == 0:
+                prev = nx
+            else:
+                prev = layers[layer-1]
+            W = np.random.randn(layers[layer], prev)*np.sqrt(2 / prev)
+            b = np.zeros((layers[layer], 1))
+
+            self.weights[f"W{layer+1}"] = W
+            self.weights[f"b{layer+1}"] = b
