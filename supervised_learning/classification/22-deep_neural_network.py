bcb206ee63938e284c5ba2d9f913262324bdec52
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-15 17:57:56 +0400

    Task 22: Train Deep NN

---
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""Deep Neural Network"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ A deep neural network consists of multiple hidden layers """
+    def __init__(self, nx, layers):
+        if not (isinstance(nx, int)):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not (isinstance(layers, list)) or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for layer in range(self.__L):
+            if layers[layer] <= 0 or not (isinstance(layers[layer], int)):
+                raise TypeError("layers must be a list of positive integers")
+            if layer == 0:
+                prev = nx
+            else:
+                prev = layers[layer-1]
+            W = (np.random.randn(layers[layer], prev) *
+                 np.sqrt(2 / prev))
+            b = np.zeros((layers[layer], 1))
+            self.__weights.update({f"W{layer+1}": W})
+            self.__weights.update({f"b{layer+1}": b})
+
+    def forward_prop(self, X):
+        """ Calculates the activation of each neuron
+        Args:
+            X -> ndarray (nx, m), where nx = features; m = examples
+        Returns:
+            Y, cache
+        """
+        prev = X
+        for layer in range(self.__L+1):
+            self.__cache[f"A{layer}"] = prev
+            if layer != self.__L:
+                z = self.__weights[f"W{layer+1}"] @ prev \
+                    + self.__weights[f"b{layer+1}"]
+                prev = self.sigmoid(z)
+        return self.__cache[f"A{self.__L}"], self.__cache
+
+    def cost(self, Y, A):
+        """ Calculates the cost using binary-cross entropy loss """
+        losses = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        return -np.mean(losses)
+
+    def evaluate(self, X, Y):
+        """ Evaluate the neural network's predictions """
+        A = self.forward_prop(X)[0]
+        preds = np.where(A >= 0.5, 1, 0)
+        return preds, self.cost(Y, A)
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neural network """
+        m = Y.shape[1]
+        A_last = cache[f"A{self.__L}"]
+        dZ = A_last - Y
+
+        for layer in range(self.__L, 0, -1):
+            A_prev = cache[f"A{layer-1}"]
+            Wl = self.__weights[f"W{layer}"]
+
+            dW = (1 / m) * np.matmul(dZ, A_prev.T)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+            self.__weights[f"W{layer}"] = (self.__weights[f"W{layer}"] -
+                                           alpha * dW)
+            self.__weights[f"b{layer}"] = (self.__weights[f"b{layer}"] -
+                                           alpha * db)
+
+            if layer > 1:
+                A_prev_layer = cache[f"A{layer-1}"]
+                dZ = np.dot(Wl.T, dZ) * (A_prev_layer * (1 - A_prev_layer))
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Trains the deep neural network by executing forward propagation
+        and gradient descent over a specified number of iterations.
+        """
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+
+        for _ in range(iterations):
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+
+        return self.evaluate(X, Y)
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
