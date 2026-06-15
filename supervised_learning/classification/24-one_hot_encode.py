be068c11bd85e4f328553cb90df44f8535ff9804
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-06-15 18:08:28 +0400

    Task 24: One-hot encoding

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""One-hot encoding"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ encode Y """
+    if not (isinstance(Y, np.ndarray)):
+        return None
+    if not (isinstance(classes, int)):
+        return None
+    if classes < 2:
+        return None
+    if classes < np.max(Y):
+        return None
+    res = np.zeros((len(Y), classes))
+    for i in range(len(Y)):
+        temp = np.zeros((1, classes))
+        temp[0, Y[i]] = 1
+        res[i] = temp
+    return res.T
