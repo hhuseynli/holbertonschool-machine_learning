5dd7e69069de9f4627ac9407c13bd397ea89c160
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-13 23:45:14 +0400

    Task 12: Test the model

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,12 @@
+"""12-test.py"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Evaluate model performance"""
+    return network.evaluate(
+        x=data,
+        y=labels,
+        verbose=verbose
+    )
