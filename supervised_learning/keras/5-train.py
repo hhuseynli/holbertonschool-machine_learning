40b1252bc4a7f087bfba004d16f18f1688faaeab
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-13 21:19:57 +0400

    Task 5: Train and validate (fit)

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Train a model """
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """ Train the model """
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
+    return history
