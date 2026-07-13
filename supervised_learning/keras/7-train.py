d4d137ef63b537c06cc368f6bfe9f2c7278deb3b
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-13 23:27:30 +0400

    Task 7: Train model with learning rate decay

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""6-train.py."""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """train_model."""
+    callbacks = []
+
+    if validation_data is not None:
+        if early_stopping:
+            callbacks.append(
+                K.callbacks.EarlyStopping(
+                    monitor='val_loss',
+                    patience=patience
+                )
+            )
+
+        if learning_rate_decay:
+            def scheduler(epoch):
+                return alpha / (1 + decay_rate * epoch)
+            callbacks.append(
+                K.callbacks.LearningRateScheduler(
+                    scheduler,
+                    verbose=1
+                )
+            )
+
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        callbacks=callbacks,
+        verbose=verbose,
+        shuffle=shuffle
+    )
