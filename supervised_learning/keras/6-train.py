5af59b8ccd205459d68f9ae1c4d09481b1e8e11b
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-13 21:39:17 +0400

    fix: update docstrings and improve code readability in train_model function

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-""" Train a model """
+"""6-train.py."""
 
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False,
-                patience=0, verbose=True, shuffle=False):
-    """ Train the model """
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """train_model."""
     callbacks = []
-    if early_stopping and validation is not None:
+
+    if early_stopping and validation_data is not None:
         callbacks.append(
             K.callbacks.EarlyStopping(
                 monitor='val_loss',
                 patience=patience
             )
         )
-    history = network.fit(
-        data,
-        labels,
+
+    return network.fit(
+        x=data,
+        y=labels,
         batch_size=batch_size,
         epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle,
         validation_data=validation_data,
-        callbacks=callbacks
+        callbacks=callbacks,
+        verbose=verbose,
+        shuffle=shuffle
     )
-    return history
