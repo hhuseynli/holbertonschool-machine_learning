332e25fa15aaa6dfee5ce45aee9fe3211a49447a
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-15 19:01:27 +0400

    Fix calculation of second moment in Adam optimizer

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -7,7 +7,7 @@ import numpy as np
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """ Returns updated variable and new first, second moments """
     v = beta1 * v + (1 - beta1) * grad
-    s = beta2 * s + (1 - beta2) * grad
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
 
     v_corr = v / (1 - beta1 ** t)
     s_corr = s / (1 - beta2 ** t)
