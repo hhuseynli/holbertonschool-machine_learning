b82f767d05aba346bc47b1ff43d549d6a60e9180
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-15 18:59:59 +0400

    Task 9: Adam (np)

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Adam implemented using numpy """
+
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """ Returns updated variable and new first, second moments """
+    v = beta1 * v + (1 - beta1) * grad
+    s = beta2 * s + (1 - beta2) * grad
+
+    v_corr = v / (1 - beta1 ** t)
+    s_corr = s / (1 - beta2 ** t)
+
+    var -= alpha * v_corr / (np.sqrt(s_corr) + epsilon)
+
+    return var, v, s
