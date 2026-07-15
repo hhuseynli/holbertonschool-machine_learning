8b0c86cc39e607af16f8127f2472704dbd4d322d
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-15 19:04:11 +0400

    Task 10: Adam (keras)

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Adam implemented using keras """
+
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """ Returns Adam optimizer """
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
