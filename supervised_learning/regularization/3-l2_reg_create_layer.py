5c7a6d6344e14ab92bca6c74bac6032ab1b9d56b
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-22 20:08:38 +0400

    Task 3: Create regularization layer

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Create a regularization layer """
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """ Create an L2 layer using tf """
+    layer = tf.keras.layers.Dense(
+        n,
+        activation=activation,
+        kernel_regularizer=tf.keras.regularizers.L2(lambtha)
+    )
+    return layer(prev)
