38cc96e2f5eba4b12e0fa4495551ff68cd69c117
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-07-22 19:32:54 +0400

    Task 0: L2 Regularization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -6,6 +6,8 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ Returns the cost of the network accounting for L2 regularization """
-    for l in range(1, L+1):
-        cost += lambtha * np.sum(weights[f"W{l}"]) ** 2 / (2 * m)
-    return cost
+    frobenius = 0
+    for layer in range(1, L+1):
+        frobenius += np.sum(weights[f"W{layer}"] ** 2)
+    reg_term = lambtha * frobenius / (2 * m)
+    return cost + reg_term
